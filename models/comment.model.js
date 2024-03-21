22c5c66ca70d4b21b2b8f22ec1cf54a805d1a406
mezuabir <mesamparker.1998@gmail.com>
2024-03-21 04:27:13 -0700

    test

--- models/comment.model.js
+++ models/comment.model.js
@@ -4,7 +4,7 @@ const commentSchema = new mongoose.Schema({
 
     tweetId :{
         type : mongoose.Schema.Types.ObjectId,
-        ref : "Tweet"
+        ref : "Tweet" 
     },
     userId: {
       type: mongoose.Schema.Types.ObjectId,
